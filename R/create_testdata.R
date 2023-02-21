d04cb05226f3ef0004828cc7ec4c5f5c65492922
Bonnie Robert <bonnie.robert@gov.bc.ca>
2023-02-20 19:56:36 -0800

    add licence and code comments

--- R/create_testdata.R
+++ R/create_testdata.R
@@ -1,3 +1,15 @@
+# Copyright 2023 Province of British Columbia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
 #' Generate a simulated dataset
 #'
 #' @details Generate a dataset of any given size based on a pdf or pmf stored
@@ -21,32 +33,45 @@
 #' @export
 generate_testdata <- function(folder_location = './data', name, dataset_size = 1000){
 
+  # List files containing the empirical data on each column.  Strip the ".csv" extension in each filename
   cols <- list.files(glue::glue("{folder_location}/{name}/distributions/empirical/"))
   cols <- stringr::str_replace(cols, ".csv", "")
 
+  # Initialize a data frame `test.df` with a single id column
   test.df <- data.frame(dummy = 1:dataset_size)
+  
+  # Initialize a data frame`cols.df` 
   cols.df <- data.frame(name = "dummy", coltype = "n")
 
+  # Loop over each file name
   for(i in cols){
+    
+    # Read the empirical distribution data from the current file
     print(glue::glue("processing column {i}"))
     empirical.df <- readr::read_csv(glue::glue("{folder_location}/{name}/distributions/empirical/{i}.csv"), 
                                     show_col_types = FALSE,  n_max = 10e6)
 
-    #handle case where empirical.df$x has one level
-    #as we can't draw a sample from a population of 1
+    
+    # If the empirical data is empty, generate some data
     if(length(empirical.df$distribution.x) == 0){
       x_emp <- sample(1:dataset_size, dataset_size, replace = FALSE)
+    # If the empirical data has one level, generating a factor vector of that level only
     } else if(length(empirical.df$distribution.x) == 1) {
       x_emp <- factor(rep(empirical.df$distribution.x, dataset_size), exclude = NULL)
+    # Generate a random sample from the empirical data using associated probabilities
     } else {
       x_emp <- sample(empirical.df$distribution.x, dataset_size, replace = TRUE, prob = empirical.df$distribution.p) #empirical x_vals. 
     }
   
+    # Add a new column to `test.df`
     test.df <- test.df %>% dplyr::mutate({{i}} := x_emp)
+    
+    # Add a new row to`cols.df`
     cols.df <- cols.df %>% rbind(cbind(name = i, coltype = min(empirical.df$value)))
   
   }
   
+  # write test.df and cols.df for later reference
   readr::write_csv(test.df, glue::glue("{folder_location}/{name}/test_df_{dataset_size}.csv"))
   readr::write_csv(cols.df, glue::glue("{folder_location}/{name}/cols_df_{dataset_size}.csv"))
 
--- R/vis_sim.R
+++ R/vis_sim.R
@@ -1,4 +1,16 @@
-#' Title
+# Copyright 2023 Province of British Columbia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+#' plot simulated data against original data
 #'
 #' @param data simulated data
 #' @param test_data original data
@@ -14,22 +26,30 @@
 #' @importFrom dplyr %>%
 vis_sim <- function(data, test_data, cols){
   for(i in cols){
+    
+    # Extract theoretical data in variable `i`
     x <- data %>% 
          dplyr::pull(i)
     
+    # Extract empirial data in variable`i`
     x_test <- test_data %>% 
               dplyr::pull(i)
     
     #we will want to expand this across all values
     cat(paste0("processing column: ", i, "\n"))
     
+    # Generate a theoretical distribution plot for the theoretical data
     g.th  <- plot_distribution(x, limits = scale_limits(x), 
                                ttl = paste0(i, ": Theoretical Distribution"))
+    
+    # Generate an empirical distribution plot for the empirical data
     g.emp <- plot_distribution(x_test, limits = scale_limits(x), 
                                ttl = paste0(i, ": Empirical Distribution"))
     
+    # Arrange the two plots in a grid
     gridExtra::grid.arrange(rbind(g.th, g.emp))
     
+    # Display and pause 5 seconds before plotting next variable
     Sys.sleep(5)
   }
 }
